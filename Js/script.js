1953bca006a2b24807187fa25a929ff87cd5f2f1
Miguel <miguelromanidejesus258@gmail.com>
2026-05-18 16:21:09 -0300

    Primeiro commit

---
+++ Js/script.js
@@ -0,0 +1,17 @@
+function toogleMode(){
+  //proucurar a tag html
+  const html = document.documentElement
+  //proucura a imagem
+  const img = document.querySelector("#profile img")
+  // o atributo "toogle" ele ja faz o processo if, else(fundo)
+  html.classList.toggle("light")
+  if (html.classList.contains("light")) {
+    //se tiver light troca a imagem
+    img.setAttribute("src", "../assets/avatar-light.png")
+    img.setAttribute("alt", "Uma foto de um cara de óculos escuro no fundo azul e meio branco")
+  } else {
+    // se não tiver light permança a imagem
+    img.setAttribute("src", "../assets/avatar.png")
+    img.setAttribute("alt", "uma foto de um cara sorrindo em um fundo amarelo")
+  }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Projeto-1
+# Projeto-1
+# Projeto-1
